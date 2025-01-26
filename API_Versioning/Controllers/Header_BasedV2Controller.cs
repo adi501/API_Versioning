26d84dffb0a449a05c77a930189e955497bb127f
Adinarayana Reddy JC <jc.adi101@gmail.com>
2025-01-26 22:07:14 +0530

    Header Based Versioning

---
+++ API_Versioning/Controllers/Header_BasedV2Controller.cs
@@ -0,0 +1,22 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Versioning.Controllers
+{
+
+    //'https://localhost:7177/api/Header_Based' \
+    //-H 'X-Version: 2'  (it's in Header section )
+    [Route("api/Header_Based")]
+    [ApiVersion("2.0")]
+    [ApiController]
+    public class Header_BasedV2Controller : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult(" from v2 controller");
+        }
+    }
+}
