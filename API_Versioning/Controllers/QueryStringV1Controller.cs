3df8767c4d9055b80511ee68b976c2d7c86b6b29
Adinarayana Reddy JC <jc.adi101@gmail.com>
2025-01-26 21:34:01 +0530

    QueryString based URL Routing

---
+++ API_Versioning/Controllers/QueryStringV1Controller.cs
@@ -0,0 +1,22 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Versioning.Controllers
+{
+    //https://localhost:7177/api/QueryString?api-version=1
+    //https://localhost:7177/api/QueryString
+
+    [Route("api/QueryString")]
+    [ApiController]
+    public class QueryStringV1Controller : ControllerBase
+    {
+        [HttpGet]
+        [ApiVersion("1.0")]
+       
+        public IActionResult Get()
+        {
+            return new OkObjectResult(" from v1 controller");
+        }
+    }
+}
---
+++ API_Versioning/Controllers/QueryStringV2Controller.cs
@@ -0,0 +1,20 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Versioning.Controllers
+{
+    //https://localhost:7177/api/QueryString?api-version=2
+
+    [Route("api/QueryString")]
+    [ApiController]
+    public class QueryStringV2Controller : ControllerBase
+    {
+        [HttpGet]
+        [ApiVersion("2.0")]
+        public IActionResult Get()
+        {
+            return new OkObjectResult(" from v2 controller");
+        }
+    }
+}
